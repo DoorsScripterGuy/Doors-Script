09e2bad421573d0b91956d059e71fa54d437ed4b
DoorsScripterGuy <121165939+DoorsScripterGuy@users.noreply.github.com>
2022-12-21 02:54:37 -0800

    merry christmas
    ahhh

---
+++ Message.lua
@@ -0,0 +1,7 @@
+firesignal(game.ReplicatedStorage.Bricks.Caption.OnClientEvent, "Merry Christmas Player!")
+wait(2)
+firesignal(game.ReplicatedStorage.Bricks.Caption.OnClientEvent, "Here is 500000 Gold")
+wait(2)
+firesignal(game.ReplicatedStorage.Bricks.Caption.OnClientEvent, "Enjoy Fatty Player Balls")
+wait(2)
+game.Players.LocalPlayer.Gold.Value = 500000
